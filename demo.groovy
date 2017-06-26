2436d68b576e5733f0d3345703b469cd1591256f
ahasnaini <ahasnaini@users.noreply.github.com>
2017-06-26 10:23:48 +0100

    Update demo.groovy

--- demo.groovy
+++ demo.groovy
@@ -4,13 +4,6 @@ job('GoKubeDemo/Demo-Multibranch') {
          scm {
             github('ahasnaini/gokubedemo', 'development')
              }
-
-            orphanedItemStrategy {
-                discardOldItems {
-                    numToKeep(5)
-                }
-            }
-
             triggers {
                 // run once a day if not otherwise run
                 periodic(1440)
