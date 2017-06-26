e4e4882bfa20120749f7cb1c69c0a33ec8b6caf7
ahasnaini <ahasnaini@users.noreply.github.com>
2017-06-26 10:24:42 +0100

    Update demo.groovy

--- demo.groovy
+++ demo.groovy
@@ -4,8 +4,4 @@ job('GoKubeDemo/Demo-Multibranch') {
          scm {
             github('ahasnaini/gokubedemo', 'development')
              }
-            triggers {
-                // run once a day if not otherwise run
-                periodic(1440)
-            }
   }
