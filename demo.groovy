211b5648b0d02843072eb8b42fcaef7e8718bb81
ahasnaini <ahasnaini@users.noreply.github.com>
2017-07-06 08:17:13 +0100

    Update demo.groovy

--- demo.groovy
+++ demo.groovy
@@ -1,18 +1,16 @@
 folder('GoKubeDemo')
 
-deliveryPipelineView('GoKubeDemo/Development') {
+deliveryPipelineView('GoKubeDemo/Production') {
     pipelineInstances(5)
     showAggregatedPipeline()
     columns(2)
     sorting(Sorting.TITLE)
-    updateInterval(60)
+    updateInterval(2)
     enableManualTriggers()
     showAvatars()
     showChangeLog()
     pipelines {
-        component('Sub System A', 'compile-a')
-        component('Sub System B', 'compile-b')
-        regex(/compile-subsystem-(.*)/)
+        component('Development', 'GoKubeDemo/PrepareDeploy')
     }
 }
 
